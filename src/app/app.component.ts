c9c68c27b0b2109a00cab766ee6b4246b6190f74
Muhammad Ahsan Ayaz <ahsanayaz@Muhammads-MacBook-Pro.local>
2021-01-14 13:10:44 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = "Creating custom Form Controls in Angular";
+}
